f5a3d4c1167e481d61f5ce743cbebff1d407ea21
Dayron Lucero <dayluccor@alu.edu.gva.es>
2026-04-21 10:18:42 +0200

    Estructura basica para las Entidades(Jugador,Enemigo,Item) y creacion de carpetas

---
+++ Projecto  Final/Entidades/Enemigo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto__Final.Entidades
+{
+    internal class Enemigo: Entidad
+    {
+        int nivelDificultad;
+        int cantidadOro;
+        int experienciaOtorgada;
+        public Enemigo(int vida, string nombre, int nivelDificultad = 0, int cantidadOro = 0, int experienciaOtorgada = 0) : base(vida, nombre)
+        {
+            this.nivelDificultad = nivelDificultad;
+            this.cantidadOro = cantidadOro;
+            this.experienciaOtorgada = experienciaOtorgada;
+        }
+        public override void Atacar(Entidad objetivo)
+        {
+            // Lógica de ataque del enemigo
+        }
+        public int NivelDificultad { get => nivelDificultad; set => nivelDificultad = value; }
+        public int CantidadOro { get => cantidadOro; set => cantidadOro = value; }
+        public int ExperienciaOtorgada { get => experienciaOtorgada; set => experienciaOtorgada = value; }
+    }
+}
