24a7a5b9c7838152ea1af9c830c4dbef3563488b
Samuel <sammarala@alu.edu.gva.es>
2026-04-21 09:24:55 +0200

    Creada la clase botón dentro de la carpeta 'Menú Principal'

--- Projecto  Final/Game1.cs
+++ Projecto  Final/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Projecto__Final
 {
@@ -8,6 +9,18 @@ namespace Projecto__Final
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        // Atributos menú
+
+        public enum GameState
+        {
+            MenuPrincipal,
+            Opciones,
+            Jugando,
+            Pausa
+        }
+
+        private GameState estadoActual = GameState.MenuPrincipal;
 
         public Game1()
         {
@@ -37,7 +50,45 @@ namespace Projecto__Final
 
             // TODO: Add your update logic here
 
+            // Cambio del estado del juego
+
+            switch (estadoActual)
+            {
+                case GameState.MenuPrincipal:
+                    UpdateMenu();
+                    break;
+                case GameState.Opciones:
+                    UpdateOpciones();
+                    break;
+                case GameState.Jugando:
+                    UpdateJuego();
+                    break;
+                case GameState.Pausa:
+                    UpdatePausa();
+                    break;
+            }
+
             base.Update(gameTime);
+        }
+
+        private void UpdatePausa()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateJuego()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateOpciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateMenu()
+        {
+            throw new NotImplementedException();
         }
 
         protected override void Draw(GameTime gameTime)
@@ -46,7 +97,45 @@ namespace Projecto__Final
 
             // TODO: Add your drawing code here
 
+            // Dependiendo del estado se dibuja una cosa u otra
+
+            switch (estadoActual)
+            {
+                case GameState.MenuPrincipal:
+                    DrawMenu();
+                    break;
+                case GameState.Opciones:
+                    DrawOpciones();
+                    break;
+                case GameState.Jugando:
+                    DrawJuego();
+                    break;
+                case GameState.Pausa:
+                    DrawPausa();
+                    break;
+            }
+
             base.Draw(gameTime);
+        }
+
+        private void DrawMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DrawOpciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DrawJuego()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DrawPausa()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Projecto  Final/Menú Principal/Boton.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto__Final
+{
+    internal class Boton
+    {
+        Texture2D _textura;
+        Vector2 _posicion;
+        Rectangle _rectangulo;
+        Color _colorActual;
+
+        private bool mouseEncima;
+        private string texto;
+
+        public bool MouseEncima { get => mouseEncima; set => mouseEncima = value; }
+        public string Texto { get => texto; set => texto = value; }
+
+        public Boton(Texture2D textura, Vector2 posicion, string texto)
+        {
+            _textura = textura;
+            _posicion = posicion;
+            Texto = texto;
+            _rectangulo = new Rectangle((int)posicion.X, (int)posicion.Y, textura.Width, textura.Height);
+        }
+
+        public void Update(MouseState mouse)
+        {
+            if (_rectangulo.Contains(mouse.X, mouse.Y))
+            {
+                MouseEncima = true;
+                _colorActual = Color.Gray;
+            }
+            else
+            {
+                MouseEncima = false;
+                _colorActual = Color.White;
+            }
+        }
+
+        public bool Clicado(MouseState mouse)
+        {
+            return MouseEncima && mouse.LeftButton == ButtonState.Pressed;
+        }
+
+        public void Draw(SpriteBatch spriteBach)
+        {
+            spriteBach.Draw(_textura, _posicion, _colorActual);
+        }
+    }
+}
