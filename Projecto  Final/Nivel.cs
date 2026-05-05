459a814ca17366f8a46cf4659b8f67f5cf654cd8
Lucía Navarro Cruz <lucnavcru@alu.edu.gva.es>
2026-05-05 14:38:05 +0200

    Retocando lógica de cofres además de las transiciones.

--- Projecto  Final/Nivel.cs
+++ Projecto  Final/Nivel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Projecto__Final.Objetos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,18 @@ namespace Projecto__Final
         Rectangle puerta1;
         Rectangle puerta2;
         string siguienteFondo;
+        List<Cofre> cofres;
+
+        public Nivel()
+        {
+            cofres=new List<Cofre>();
+        }
 
         public Texture2D Fondo { get => fondo; set => fondo = value; }
         public Texture2D Colisiones { get => colisiones; set => colisiones = value; }
         public Rectangle Puerta1 { get => puerta1; set => puerta1 = value; }
         public string SiguienteFondo { get => siguienteFondo; set => siguienteFondo = value; }
         public Rectangle Puerta2 { get => puerta2; set => puerta2 = value; }
+        public List<Cofre> Cofres { get => cofres; set => cofres = value; }
     }
 }
